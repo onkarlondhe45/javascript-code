74799ff1ac792907921cc2575cf1dc6009fbd2fa
onkarlondhe <onkarrameshlondhe@gmail.com>
2024-04-28 18:31:39 +0530

    #3 index and script files added.

---
+++ script-3.js
@@ -0,0 +1,88 @@
+
+document.write("2==2  \n",2==2);
+
+document.write("<br>");
+
+document.write("2==3  \n",2==3);
+
+document.write("<br>");
+
+document.write("2=='2' \n",2=="2");
+
+document.write("<br>");
+
+document.write("2==='2'\n",2==="2");
+
+document.write("<br>");
+
+document.write("2!=0 \n", 2!=0);
+
+document.write("<br>");
+
+document.write("2!=2 \n", 2!=2);
+
+document.write("<br>");
+
+document.write("2!==0 \n", 2!==0);
+
+document.write("<br>");
+
+document.write("2!=='0' \n", 2!=='0');
+
+document.write("<br>");
+
+var a = 10;
+var b = 5;
+
+document.write(a<b);
+
+document.write("<br>");
+
+document.write(a>b);
+
+document.write("<h3>Ternery Operator</h3><p>(if we want to write if else condition in single line then we use Ternery Operator)</p>");
+
+let x = 5;
+
+let y = 10; 
+
+let z = x>y ? "x=5>y=10 this statement is true" : "x=5>y=10 this statement is false";
+document.write(z);
+
+
+document.write("<h3>String Concatenation Demo</h3>");
+
+var firstName="Onkar";
+var lastName="Londhe";
+
+var fullName = firstName + lastName;
+
+document.write("First Name : ",firstName);
+
+document.write("<br>");
+
+document.write("Last Name : ",lastName);
+
+document.write("<br>");
+
+document.write("Full Name : ",fullName);
+
+document.write("<br>");
+
+document.write("<h3>Operator</h3>");
+
+var n1 = 10;
+var n2 = 20;
+
+document.write(n1<=n2 && n1>=n2);
+
+document.write("<br>");
+
+document.write(n1<=n2 || n1>=n2);
+
+document.write("<br>");
+
+document.write("<h3>Object in JavaScript</h3>");
+
+
+
