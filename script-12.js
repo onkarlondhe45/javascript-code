a3c31ae5fba29822a182b03f1d0a22acbb2396ec
onkarlondhe <onkarrameshlondhe@gmail.com>
2024-05-05 16:15:51 +0530

    #12 events in javascript demo code added.

---
+++ script-12.js
@@ -0,0 +1,32 @@
+function onClickEvent(e){
+	console.log(e.target);
+	e.target.style.color="white";
+ }
+
+
+function onMouseOver(e){
+	e.target.style.color="yellow";
+}
+
+function onMouseDown(e){
+	e.target.style.backgroundColor="blue";
+}
+
+
+function onMouseUp(e){
+	e.target.style.backgroundColor="red";
+}
+
+function onKeyDown(e){
+	e.target.style.backgroundColor="yellow";
+}
+
+
+function onKeyUp(e){
+	e.target.style.backgroundColor="white";
+}
+
+function onChange(e){
+	document.getElementById("first").e.target.value;
+	
+}
